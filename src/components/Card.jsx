37c9c0c9e0e512704103eb203504025446df73d3
kellylee23 <dmstj4632@hufs.ac.kr>
2025-02-11 19:31:53 +0900

    feat:카드 랜덤 변수 생성

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
+import front1 from "../img/front1.png";
+import front2 from "../img/front2.png";
+import front3 from "../img/front3.png";
+import backCard from "../img/backCard.png";
 import HoroscopeText from "./HoroscopeText";
 
+const CardDiv = styled.div`
+  margin-left: 100px;
+  margin-right: 100px;
+`;
 // 카드 스타일링
 const CardWrapper = styled.div`
-  width: 100px;
-  height: 150px;
-  background-color: #4caf50;
+  width: 200px;
+  height: 330px;
   color: white;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 0 10px;
-  border-radius: 8px;
+  margin: 100px 10px;
   cursor: pointer;
+  position: relative;
   transition: transform 1.5s ease, opacity 0.5s ease;
 
-  /* 열린 카드만 커지면서 왼쪽으로 이동 */
-  ${({ isOpen }) =>
+  /* 뒷면 이미지 */
+  background-image: url(${backCard});
+  background-size: cover;
+  background-position: center;
+
+  /* 카드가 뒤집히면 앞면 이미지가 보이도록  + 카드 왼쪽 이동*/
+  ${({ isOpen, frontImage }) =>
     isOpen &&
     `
+    background-image: url(${frontImage});
+    transform: rotateY(180deg); /* 카드를 180도 회전시켜서 앞면을 보여줌 */
     transform: scale(1.5) translateX(-150px);  /* 커지면서 왼쪽으로 이동 */
   `}
 
-  /* 열린 카드가 뒤집히는 애니메이션 */
-  ${({ isOpen }) =>
-    isOpen &&
-    `
-    transform: scale(1.5) translateX(-150px) rotateY(180deg);  /* 커지면서 왼쪽으로 이동하고 뒤집힘 */
-  `}
-
   /* 열린 카드를 제외한 나머지 카드들은 점점 작아지고 사라짐 */
-  ${({ isNotOpen }) =>
+   ${({ isNotOpen }) =>
     isNotOpen &&
     `
-    transform: scale(0.3);  /* 점점 작아지도록 설정 */
-    opacity: 0;  /* 점점 투명해지도록 설정 */
-  `}
+     transform: scale(0.3);  /* 점점 작아지도록 설정 */
+     opacity: 0;  /* 점점 투명해지도록 설정 */
+   `}
 `;
 
 const CardContainer = styled.div`
@@ -47,24 +53,22 @@ const CardContainer = styled.div`
   flex-direction: row;
 `;
 
-function Card({ cardId, index, openedCard, handleCardClick }) {
+const Card = ({ cardId, index, openedCard, handleCardClick, frontImage }) => {
   const isOpen = openedCard === index; // 클릭된 카드가 펼쳐졌는지 확인
-  const isNotOpen = openedCard !== null && openedCard !== index; // 열린 카드 제외 나머지 카드들
+  const isNotOpen = openedCard !== null && openedCard !== index; // 열린 카드 제외 나머지 카드들 사라짐짐
 
   return (
     <CardContainer>
       <CardWrapper
-        isOpen={isOpen}
-        isNotOpen={isNotOpen} // 열린 카드 제외 나머지 카드들에 스타일 적용
+        isOpen={isOpen} // 카드가 열렸을 때 앞면 보이도록
+        isNotOpen={isNotOpen} //나머지 카드 사라짐
+        frontImage={frontImage} // 랜덤 앞면 이미지
         onClick={() => handleCardClick(index)} // 카드 클릭 시 상태 변경
-      >
-        <p>카드 {cardId}</p>
-      </CardWrapper>
-
+      ></CardWrapper>
       {/* 카드가 열린 상태면, 운세 텍스트 표시 */}
       {isOpen && <HoroscopeText />}
     </CardContainer>
   );
-}
+};
 
 export default Card;
