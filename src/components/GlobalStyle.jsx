c99f4d48eeca4c8e76c730420a810d098735ebce
Seung876 <sm08073469@gmail.com>
2025-02-20 02:12:29 +0900

    배경설정

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'SDSamliphopangche_Outline';
+    src: url('./fonts/SDSamliphopangcheTTFOutline.ttf') format('truetype');  
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+
+export default GlobalStyle;
