3784fca93d583f010deb7a23220ae7f2a5787913
Seung876 <sm08073469@gmail.com>
2025-02-23 20:16:29 +0900

    이동경로

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { useEffect } from "react";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import StartPage from "./pages/StartPage";
 
 import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.onbeforeunload = () => {
+      sessionStorage.setItem("shouldNavigateToStart", "true");
+    };
+
+    // 새로고침 후 StartPage로 이동
+    if (sessionStorage.getItem("shouldNavigateToStart") === "true") {
+      navigate("/");
+      sessionStorage.removeItem("shouldNavigateToStart");
+    }
+  }, [navigate]);
+
   return (
     <div>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/main" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default function AppWithRouter() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
