0908f300a484a3944016e3db9ee3f9502a24c393
kellylee23 <dmstj4632@hufs.ac.kr>
2025-02-23 17:16:50 +0900

    폰트적용

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -2,10 +2,21 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: 'SDSamliphopangche_Outline';
-    src: url('./fonts/SDSamliphopangcheTTFOutline.ttf') format('truetype');  
+    font-family: '42dot Sans';
+    src: url('/fonts/42dotSans-Medium.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
+  }
+  @font-face {
+    font-family: 'Paperlogy-8ExtraBold';
+    src: url('/fonts/Paperlogy-8ExtraBold.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: '42dot Sans', sans-serif;
   }
 `;
 
